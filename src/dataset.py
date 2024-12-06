989b213d4e34207bf9fa90dfb681dfd76b33f358
Ahmet Talha Biçer <bicerahmettalha@gmail.com>
2024-12-07 00:55:30 +0300

    add data

---
+++ src/dataset.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data ={
+    'name': ['John', 'Anna', 'Peter', 'Linda'],
+    'age': [23, 45, 32, 25],
+    'city': ['New York', 'Paris', 'Berlin', 'London']
+}
+
+df = pd.DataFrame(data)
